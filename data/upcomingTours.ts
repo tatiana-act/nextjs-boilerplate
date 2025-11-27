8a391be557de7a7a33c6284d6ad2493a70061b06
Eugene Potapov <epotapov@zello.com>
2025-11-26 22:13:18 -0600

    remove dec03

--- data/upcomingTours.ts
+++ data/upcomingTours.ts
@@ -2,14 +2,6 @@ import { UpcomingTourEvent } from '@/types/tour';
 
 export const upcomingTours: UpcomingTourEvent[] = [
     {
-        id: 'tour28',
-        tourProgramId: 'Acap',
-        date: '2025-12-03',
-        time: '10:00',
-        price: 40,
-        bonus: '️с посещением Капитолия'
-    },
-    {
         id: 'tour23',
         tourProgramId: 'Gcrt',
         date: '2025-12-05',
