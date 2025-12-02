903e389c625ae1255547d953c14240d1b65017e8
Eugene Potapov <epotapov@zello.com>
2025-12-01 23:24:15 -0600

    new tour

--- data/upcomingTours.ts
+++ data/upcomingTours.ts
@@ -42,6 +42,14 @@ export const upcomingTours: UpcomingTourEvent[] = [
         bonus: 'с посещением Шато Бельвью'
     },
     {
+        id: 'tour34',
+        tourProgramId: 'Acap',
+        date: '2025-12-16',
+        time: '10:30',
+        price: 40,
+        bonus: '️с посещением Капитолия'
+    },
+    {
         id: 'tour32',
         tourProgramId: 'Brmn',
         date: '2025-12-29',
