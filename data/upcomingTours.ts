4f54b85f00a75ec8cbfc2c52a24fc391aa44dbc1
Eugene Potapov <epotapov@zello.com>
2026-01-03 17:39:49 -0600

    jan tour updates

--- data/upcomingTours.ts
+++ data/upcomingTours.ts
@@ -18,20 +18,36 @@ export const upcomingTours: UpcomingTourEvent[] = [
         bonus: 'с посещением Капитолия'
     },
     {
-        id: 'tour43',
-        tourProgramId: 'Hyde',
+        id: 'tour44',
+        tourProgramId: 'Auhnry',
         date: '2026-01-11',
+        time: '11:00',
+        price: 40,
+        bonus: ""
+    },
+    {
+        id: 'tour48',
+        tourProgramId: 'Milt',
+        date: '2026-01-16',
         time: '11:00',
         price: 40,
         bonus: ''
     },
     {
-        id: 'tour44',
-        tourProgramId: 'Acap',
+        id: 'tour43',
+        tourProgramId: 'Hyde',
         date: '2026-01-18',
         time: '11:00',
         price: 40,
-        bonus: "с посещением музея О'Генри"
+        bonus: ''
+    },
+    {
+        id: 'tour46',
+        tourProgramId: 'Brmn',
+        date: '2026-01-23',
+        time: '10:00',
+        price: 40,
+        bonus: "с посещением Шато Бельвью"
     },
     {
         id: 'tour45',
@@ -42,11 +58,11 @@ export const upcomingTours: UpcomingTourEvent[] = [
         bonus: 'с посещением музея'
     },
     {
-        id: 'tour46',
-        tourProgramId: 'Brmn',
+        id: 'tour47',
+        tourProgramId: 'Auhnry',
         date: '2026-01-25',
         time: '11:00',
         price: 40,
-        bonus: "кофе-брейк в Phoebe's"
-    },
+        bonus: ""
+    }
 ];
