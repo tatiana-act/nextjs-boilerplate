67d928fa34ef680d97d2d61bff88945f820586f1
Eugene Potapov <epotapov@zello.com>
2026-02-18 14:21:34 -0600

    add March tours

--- data/RecentTours.ts
+++ data/RecentTours.ts
@@ -1,6 +1,28 @@
 import {PastTourEvent} from '@/types/tour';
 
-const pastTourEvents: PastTourEvent[] = [
+const pastTourEvents: PastTourEvent[] =
+    {
+        id: 'tour54',
+        tourProgramId: 'Gcrt',
+        date: '2026-02-14',
+        time: '11:00',
+        price: 40,
+        bonus: 'bonusMuseum',
+        eventUrl: '',
+        eventImage: '',
+        feedbacks: []
+    },
+    {
+        id: 'tour52',
+        tourProgramId: 'Brmn',
+        date: '2026-02-16',
+        time: '10:40',
+        price: 40,
+        bonus: 'bonusChat',
+        eventUrl: '',
+        eventImage: '',
+        feedbacks: []
+    },
     {
         id: 'tour42',
         tourProgramId: 'Gcrt',
@@ -258,81 +280,6 @@ const pastTourEvents: PastTourEvent[] = [
         eventImage: '',
         feedbacks: []
     },
-    {
-        id: 'tour7',
-        tourProgramId: 'Gcrt',
-        date: '2025-09-27',
-        time: '12:30',
-        eventUrl: 'https://www.facebook.com/groups/662224066870718/posts/721785767581214/',
-        eventImage: '/092725grt.jpg',
-        feedbacks: []
-    },
-    {
-        id: 'tour12',
-        tourProgramId: 'Acap',
-        date: '2025-09-20',
-        time: '13:00',
-        eventUrl: 'https://www.facebook.com/groups/662224066870718/posts/716853981407726/',
-        eventImage: '/092025ac.jpg',
-        feedbacks: []
-    },
-    {
-        id: 'tour8',
-        tourProgramId: 'Acap',
-        date: '2025-09-14',
-        time: '09:00',
-        eventUrl: 'https://www.facebook.com/groups/662224066870718/posts/709989568760834/',
-        eventImage: '/091425ac.jpg',
-        feedbacks: []
-    },
-    {
-        id: 'tour10',
-        tourProgramId: 'Amhry',
-        date: '2025-09-07',
-        time: '12:00',
-        eventUrl: 'https://www.facebook.com/orlowa.tatiana/posts/3655321031268146',
-        eventImage: '/090725amhry.jpg',
-        feedbacks: []
-    },
-    {
-        id: 'tour9',
-        tourProgramId: 'Gcrt',
-        date: '2025-09-06',
-        time: '11:30',
-        eventUrl:
-            'https://www.facebook.com/groups/662224066870718/posts/703564966069961/',
-        eventImage: '/090625grt.jpg',
-        feedbacks: []
-    },
-    {
-        id: 'tour4',
-        tourProgramId: 'Gcrt',
-        date: '2025-08-17',
-        time: '12:00',
-        eventUrl:
-            'https://www.facebook.com/groups/662224066870718/posts/686627174430407/',
-        eventImage: '/081725grt.jpg',
-        feedbacks: [],
-    },
-    {
-        id: 'tour2',
-        tourProgramId: 'Gcrt',
-        date: '2025-08-16',
-        time: '10:00',
-        eventUrl:
-            'https://www.facebook.com/groups/662224066870718/posts/686551121104679',
-        eventImage: '/081625grt.jpg',
-        feedbacks: [],
-    },
-    {
-        id: 'tour1',
-        tourProgramId: 'Acap',
-        date: '2025-07-27',
-        time: '10:00',
-        eventUrl: 'https://www.facebook.com/groups/662224066870718/posts/670617699364688/',
-        eventImage: '/072725ac.jpg',
-        feedbacks: [],
-    }
 ];
 
 export default pastTourEvents;
--- data/upcomingTours.ts
+++ data/upcomingTours.ts
@@ -2,35 +2,19 @@ import { UpcomingTourEvent } from '@/types/tour';
 
 export const upcomingTours: UpcomingTourEvent[] = [
   {
-    id: 'tour54',
-    tourProgramId: 'Gcrt',
-    date: '2026-02-14',
-    time: '11:00',
+    id: 'tour53',
+    tourProgramId: 'Hyde',
+    date: '2026-03-02',
+    time: '10:30',
     price: 40,
-    bonus: 'bonusMuseum',
+    bonus: '',
   },
   {
     id: 'tour55',
-    tourProgramId: 'Gcrt',
-    date: '2026-02-15',
-    time: '11:00',
-    price: 40,
-    bonus: '',
-  },
-  {
-    id: 'tour52',
     tourProgramId: 'Brmn',
-    date: '2026-02-16',
-    time: '10:40',
+    date: '2026-03-09',
+    time: '11:10',
     price: 40,
     bonus: 'bonusChat',
-  },
-  {
-    id: 'tour53',
-    tourProgramId: 'Hyde',
-    date: '2026-02-17',
-    time: '11:00',
-    price: 40,
-    bonus: '',
   },
 ];
