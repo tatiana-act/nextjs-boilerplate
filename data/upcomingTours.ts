d77a161ef5b7f4b317cd59ac6589f879c759b2cd
Eugene Potapov <epotapov@zello.com>
2026-03-05 21:15:11 -0600

    update tour in calendar

--- data/upcomingTours.ts
+++ data/upcomingTours.ts
@@ -13,7 +13,7 @@ export const upcomingTours: UpcomingTourEvent[] = [
     id: 'tour53',
     tourProgramId: 'Hyde',
     date: '2026-03-10',
-    time: '10:00',
+    time: '09:30',
     price: 40,
     bonus: '',
   },
